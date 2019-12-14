95480ffc5695738dca128e5466d6e68b68a1850f
jun <vagrantsj@gmail.com>
2019-12-14 13:31:26 +0900

    실제 위치에 맞는 데이터 출력

---
+++ app/src/main/java/com/example/sj20191214_01_listviewpractice02/GameDetailActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sj20191214_01_listviewpractice02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameDetailActivity : BaseActivity() {
+    override fun setUpEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_detail)
+    }
+}
--- app/src/main/java/com/example/sj20191214_01_listviewpractice02/MainActivity.kt
+++ app/src/main/java/com/example/sj20191214_01_listviewpractice02/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.sj20191214_01_listviewpractice02
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.UserHandle
+import android.widget.Toast
 import com.example.sj20191214_01_listviewpractice02.adepters.GameAdepter
 import com.example.sj20191214_01_listviewpractice02.datas.GameData
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.ArrayList
 
 class MainActivity : BaseActivity() {
@@ -19,6 +22,26 @@ class MainActivity : BaseActivity() {
         setValues()
     }
     override fun setUpEvents() {
+
+        gameListView.setOnItemLongClickListener { parent, view, position, id ->
+
+            val clickedGameData = gameList.get(position)
+
+            Toast.makeText(mContext, "${clickedGameData.title} 을 삭제하려고 함", Toast.LENGTH_SHORT).show()
+
+            return@setOnItemLongClickListener true
+        }
+
+        gameListView.setOnItemClickListener { parent, view, position, id ->
+
+            val clickedGameData = gameList.get(position)
+            Toast.makeText(mContext, "${clickedGameData.title} 클릭", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(mContext, GameDetailActivity::class.java)
+            intent.putExtra("gameData", clickedGameData)
+            startActivity(intent)
+
+        }
     }
 
     override fun setValues() {
@@ -27,14 +50,18 @@ class MainActivity : BaseActivity() {
         gameListView.adapter = gameAdepter
     }
 
-    fun addGames(){
-        gameList.add(GameData("아스팔트8 : 어어본", "gameLoft"))
-        gameList.add(GameData("아스팔트7 : 어어본", "gameComp7"))
-        gameList.add(GameData("아스팔트6 : 어어본", "gameComp6"))
-        gameList.add(GameData("아스팔트5 : 어어본", "gameComp5"))
-        gameList.add(GameData("아스팔트4 : 어어본", "gameComp4"))
-
+    fun addGames() {
+        gameList.add(GameData("아스팔트 8 : 에어본", "Gameloft"))
+        gameList.add(GameData("MineCraft - Pocket Edition", "Mojang"))
+        gameList.add(GameData("아스팔트 7 : 히트", "Gameloft"))
+        gameList.add(GameData("팔라독(Paladog)", "FazeCat"))
+        gameList.add(GameData("Plants Vs. Zombie", "EA Swiss Sarl"))
+        gameList.add(GameData("스왐피 (Swampy)", "Disney"))
     }
 
+
+}
+
+private fun Intent.putExtra(s: String, clickedGameData: GameData) {
 
 }
